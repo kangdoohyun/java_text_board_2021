fb083b72146cb61552014fbe85a2ff7b91d86a89
kangdoohyun <rkdengus1208@gmail.com>
2021-06-24 19:43:46 +0900

    수정, 삭제 기능 구현

--- src/com/kdh/exam/app/App.java
+++ src/com/kdh/exam/app/App.java
@@ -115,6 +115,101 @@ public class App {
 				System.out.println("제목 : " + foundArticle.getTitle());
 				System.out.println("내용 : " + foundArticle.getBody());
 			}
+			else if(command.startsWith("/usr/article/delete")) {
+				String queryStr = "";
+				try {
+					queryStr = command.split("\\?", 2)[1];
+				}
+				catch (ArrayIndexOutOfBoundsException e) {
+					System.out.println("쿼리를 입력해 주세요");
+					continue;
+				}
+				
+				String[] queryStrBits = queryStr.split("&");
+				int id = 0;
+				for (String queryStrBit : queryStrBits) {
+					String[] queryStrBitsBits = queryStrBit.split("=", 2);
+					String paramName = queryStrBitsBits[0];
+					String paramValue = queryStrBitsBits[1];
+					
+					if(paramName.equals("id")) {
+						id = Integer.parseInt(paramValue);
+					}
+				}
+				if (id == 0) {
+					System.out.println("id를 입력해주세요");
+					continue;
+				}
+				
+				Article foundArticle = null;
+				
+				for (Article article : articles) {
+					if(article.getId() == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+				
+				if(foundArticle == null) {
+					System.out.println("존재하지 않는 게시물입니다.");
+					return;
+				}
+				
+				articles.remove(foundArticle);
+				
+				System.out.println(id + "번 게시물이 삭제되었습니다.");
+			}
+			else if(command.startsWith("/usr/article/modify")) {
+				String queryStr = "";
+				try {
+					queryStr = command.split("\\?", 2)[1];
+				}
+				catch (ArrayIndexOutOfBoundsException e) {
+					System.out.println("쿼리를 입력해 주세요");
+					continue;
+				}
+				
+				String[] queryStrBits = queryStr.split("&");
+				int id = 0;
+				for (String queryStrBit : queryStrBits) {
+					String[] queryStrBitsBits = queryStrBit.split("=", 2);
+					String paramName = queryStrBitsBits[0];
+					String paramValue = queryStrBitsBits[1];
+					
+					if(paramName.equals("id")) {
+						id = Integer.parseInt(paramValue);
+					}
+				}
+				if (id == 0) {
+					System.out.println("id를 입력해주세요");
+					continue;
+				}
+				
+				Article foundArticle = null;
+				
+				for (Article article : articles) {
+					if(article.getId() == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+				
+				if(foundArticle == null) {
+					System.out.println("존재하지 않는 게시물입니다.");
+					return;
+				}
+				
+				System.out.print("제목 : ");
+				String title = sc.nextLine();
+				System.out.print("내용 : ");
+				String body = sc.nextLine();
+				
+				foundArticle.setUpdateDate(Util.getNowDateStr());
+				foundArticle.setTitle(title);
+				foundArticle.setBody(body);
+				
+				System.out.println(id + "번 게시물이 수정되었습니다.");
+			}
 		}
 		System.out.println("== Java Text Board End ==");
 	}
